66b46470b46f56695d310d4a7b80b01429f1cb53
immanuel <immanuel1743@gmail.com>
2024-06-03 17:31:39 +0900

    버그 픽스

--- k-finance.co.kr/CCL_files/function.js
+++ k-finance.co.kr/CCL_files/function.js
@@ -39,7 +39,6 @@ async function checkboxFrom(){
       phone = $('#phone').val(),
       checkbox = $('#privacy_agreement');
       
-
   if (name === '' || name === 'undefined') {
     alert('이름을 입력해주세요.');
     $('#name').focus();
@@ -86,14 +85,12 @@ async function checkboxFrom(){
     {"key": "번호", "value":"010"+phone}
   ];
 
-
   var variable = window.consultingResult = await inputCustData(formData),
       getStatus = variable.status;
   
   if(getStatus == '200') alert('상담신청이 완료되었습니다.');
   else if (getStatus == '403') alert('비정상적인 접근입니다. 다시 시도해 주세요.'); 
   else if (getStatus == '500') alert('서버 내부 오류입니다. 다시 시도해 주세요.');
-  
 }
 
 function showPopup() {
