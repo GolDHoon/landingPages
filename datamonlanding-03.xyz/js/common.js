3ca4a65f748b5f50ead159e51cd250a566609e04
손철훈 <sch9208@gmail.com>
2024-07-10 18:23:38 +0900

    케이파이넌스 테스트

---
+++ datamonlanding-03.xyz/js/common.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    let contentboxList = document.querySelectorAll(".content-box");
+
+    for(var i = 0; i <contentboxList.length; i++){
+        contentboxList[i].style.height = contentboxList[i].offsetHeight - 4 + "px";
+    }
+
+    if(detectDevice() === "Mobile"){
+        var imgElems = document.querySelectorAll('[alt="콘텐츠01"]');
+        imgElem.src = './image/01_m.png';
+    }
+
+    window.onresize = bodyResize;
+}
+
+function bodyResize(){
+    // let contentboxList = document.querySelectorAll(".content-box");
+    //
+    //
+    // for(var i = 0; i < contentboxList.length; i++){
+    //     contentboxList[i].style.height = contentboxList[i].offsetHeight - 4 + "px";
+    // }
+
+    // var width = window.innerWidth
+    //     || document.documentElement.clientWidth
+    //     || document.body.clientWidth;
+    //
+    // var imgElems = document.querySelectorAll('[alt="콘텐츠01"]');
+    // imgElems.forEach(function(imgElem) {
+    //     if (width <= 768) {
+    //         imgElem.src = './image/01_m.png';
+    //     } else {
+    //         imgElem.src = './image/01.png';
+    //     }
+    // });
+}
+
+function detectDevice() {
+    const userAgent = navigator.userAgent;
+
+    if (/Mobi|Android/i.test(userAgent)) {
+        return "Mobile";
+    }
+    return "PC";
+}
