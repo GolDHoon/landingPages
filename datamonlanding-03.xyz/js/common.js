a8e43e31681f3df67bf8ec9fa197bdd74f53f468
손철훈 <sch9208@gmail.com>
2024-07-10 19:30:40 +0900

    케이파이넌스 테스트

--- datamonlanding-03.xyz/js/common.js
+++ datamonlanding-03.xyz/js/common.js
@@ -1,57 +1,26 @@
 window.onload = function () {
-    var width = document.querySelector('body').offsetWidth;
-
-    var imgElems = document.querySelectorAll('[alt="콘텐츠01"]');
-    imgElems.forEach(function(imgElem) {
-        if (width <= 768) {
-            imgElem.src = './image/01_m.png';
-        } else {
-            imgElem.src = './image/01.png';
-        }
-    });
-
-    let contentboxList = document.querySelectorAll(".content-box");
-
-    for(var i = 0; i <contentboxList.length; i++){
-        contentboxList[i].style.height = contentboxList[i].offsetHeight - 4 + "px";
-    }
-
-    if(detectDevice() === "Mobile"){
-        var imgElems = document.querySelectorAll('[alt="콘텐츠01"]');
-        imgElem.src = './image/01_m.png';
-    }
-
-    window.onresize = bodyResize;
-    bodyResize();
-}
-
-function bodyResize(){
-    // let contentboxList = document.querySelectorAll(".content-box");
+    // var width = document.querySelector('body').offsetWidth;
     //
+    // if (width <= 768) {
+    //     document.querySelector('[alt="콘텐츠01"]').src = './image/01_m.png';
+    //     document.querySelector('[alt="콘텐츠02"]').src = './image/02_m.png';
+    //     document.querySelector('[alt="콘텐츠03"]').src = './image/03_m.png';
+    //     document.querySelector('[alt="콘텐츠04"]').src = './image/04_m.png';
+    //     document.querySelector('[alt="콘텐츠05"]').src = './image/05_m.png';
+    //     document.querySelector('[alt="콘텐츠06"]').src = './image/06_m.png';
     //
-    // for(var i = 0; i < contentboxList.length; i++){
-    //     contentboxList[i].style.height = contentboxList[i].offsetHeight - 4 + "px";
+    // } else {
+    //     document.querySelector('[alt="콘텐츠01"]').src = './image/01.png';
+    //     document.querySelector('[alt="콘텐츠02"]').src = './image/02.png';
+    //     document.querySelector('[alt="콘텐츠03"]').src = './image/03.png';
+    //     document.querySelector('[alt="콘텐츠04"]').src = './image/04.png';
+    //     document.querySelector('[alt="콘텐츠05"]').src = './image/05.png';
+    //     document.querySelector('[alt="콘텐츠06"]').src = './image/06.png';
+    //
+    //     let contentboxList = document.querySelectorAll(".content-box");
+    //
+    //     for(var i = 0; i <contentboxList.length; i++){
+    //         contentboxList[i].style.height = contentboxList[i].offsetHeight - 4 + "px";
+    //     }
     // }
-
-    var width = window.innerWidth
-        || document.documentElement.clientWidth
-        || document.body.clientWidth;
-
-    var imgElems = document.querySelectorAll('[alt="콘텐츠01"]');
-    imgElems.forEach(function(imgElem) {
-        if (width <= 768) {
-            imgElem.src = './image/01_m.png';
-        } else {
-            imgElem.src = './image/01.png';
-        }
-    });
-}
-
-function detectDevice() {
-    const userAgent = navigator.userAgent;
-
-    if (/Mobi|Android/i.test(userAgent)) {
-        return "Mobile";
-    }
-    return "PC";
 }
