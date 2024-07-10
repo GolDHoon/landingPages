a6d9fc5ea349e87153807b01679b7629cd5db23f
손철훈 <sch9208@gmail.com>
2024-07-10 18:26:45 +0900

    케이파이넌스 테스트

--- datamonlanding-03.xyz/js/common.js
+++ datamonlanding-03.xyz/js/common.js
@@ -1,4 +1,17 @@
 window.onload = function () {
+    var width = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+
+    var imgElems = document.querySelectorAll('[alt="콘텐츠01"]');
+    imgElems.forEach(function(imgElem) {
+        if (width <= 768) {
+            imgElem.src = './image/01_m.png';
+        } else {
+            imgElem.src = './image/01.png';
+        }
+    });
+
     let contentboxList = document.querySelectorAll(".content-box");
 
     for(var i = 0; i <contentboxList.length; i++){
