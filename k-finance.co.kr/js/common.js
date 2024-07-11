34ac7326070061559c7bb4846df0a55b1bdf7851
손철훈 <sch9208@gmail.com>
2024-07-11 10:45:31 +0900

    케이파이넌스 수정

--- k-finance.co.kr/js/common.js
+++ k-finance.co.kr/js/common.js
@@ -54,84 +54,84 @@ window.onload = function () {
     });
 
 
-    let consultElement = document.querySelectorAll('.consult_button');
-
-    consultElement.forEach(element => {
-        submit().then(r => {});
-    })
+    submit().then(r => {});
 
     async function submit() {
-        element.addEventListener('click', function () {
-            if (window.blockedKeyword) {
-                var blockK = window.blockedKeyword.data;
-                var isBreak = false;
+        let consultElement = document.querySelectorAll('.consult_button');
 
-                blockK.forEach(function (word) {
-                    if (name.includes(word)) {  // checkName이 차단된 키워드를 포함하는지 확인
-                        isBreak = true;
-                        alert('차단된 키워드가 사용되었습니다. 다시 입력해 주세요. (' + name + ')');
+        consultElement.forEach(element => {
+            element.addEventListener('click', function () {
+                if (window.blockedKeyword) {
+                    var blockK = window.blockedKeyword.data;
+                    var isBreak = false;
+
+                    blockK.forEach(function (word) {
+                        if (name.includes(word)) {  // checkName이 차단된 키워드를 포함하는지 확인
+                            isBreak = true;
+                            alert('차단된 키워드가 사용되었습니다. 다시 입력해 주세요. (' + name + ')');
+                        }
+                    });
+
+                    if (isBreak === true) {
+                        return false;
                     }
-                });
-
-                if (isBreak === true) {
-                    return false;
                 }
-            }
 
-            if (name === '' || name === 'undefined') {
-                alert('이름을 입력해주세요.');
-                nameInputElement[0].focus();
-                return false;
-            } else {
-                var cho_sung_result = /^[ㄱ-ㅎ]/.test(name);
-                if (!cho_sung_result) {
-                    if (!/^[가-힣]{2,7}$/.test(name)) {
-                        alert('초성을 제외한 한글 2~7글자를 입력해주세요.');
+                if (name === '' || name === 'undefined') {
+                    alert('이름을 입력해주세요.');
+                    nameInputElement[0].focus();
+                    return false;
+                } else {
+                    var cho_sung_result = /^[ㄱ-ㅎ]/.test(name);
+                    if (!cho_sung_result) {
+                        if (!/^[가-힣]{2,7}$/.test(name)) {
+                            alert('초성을 제외한 한글 2~7글자를 입력해주세요.');
+                            nameInputElement[0].focus();
+                            return false;
+                        }
+                    } else {
+                        alert('초성을 제거해주세요.');
                         nameInputElement[0].focus();
                         return false;
                     }
-                } else {
-                    alert('초성을 제거해주세요.');
-                    nameInputElement[0].focus();
-                    return false;
                 }
-            }
 
-            if (phone === '' || phone === 'undefined') {
-                alert('휴대폰 번호를 입력해주세요.');
-                phoneInputElement[0].focus();
-                return false;
-            } else {
-                phone = phone.replace(/-/g, "");
-                if (!/^(?![01])\d{8}$/.test(phone)) {
-                    alert('정확한 휴대폰 번호를 입력해주세요.');
+                if (phone === '' || phone === 'undefined') {
+                    alert('휴대폰 번호를 입력해주세요.');
                     phoneInputElement[0].focus();
                     return false;
+                } else {
+                    phone = phone.replace(/-/g, "");
+                    if (!/^(?![01])\d{8}$/.test(phone)) {
+                        alert('정확한 휴대폰 번호를 입력해주세요.');
+                        phoneInputElement[0].focus();
+                        return false;
+                    }
                 }
-            }
 
-            if (!agree) {
-                alert('개인정보처리방침에 동의해주세요.');
-                agreeInputElement[0].focus();
-                return false;
-            }
+                if (!agree) {
+                    alert('개인정보처리방침에 동의해주세요.');
+                    agreeInputElement[0].focus();
+                    return false;
+                }
 
-            var formData = [
-                {"key": "이름", "value": name},
-                {"key": "번호", "value": "010" + phone}
-            ];
-        });
-
-        var variable = window.consultingResult = await inputCustData(formData),
-            getStatus = variable.status;
-
-        if (getStatus == '200') {
-            window.dataLayer.push({
-                event: "DB_input"
+                var formData = [
+                    {"key": "이름", "value": name},
+                    {"key": "번호", "value": "010" + phone}
+                ];
             });
-            alert('상담신청이 완료되었습니다.');
-        } else if (getStatus == '403') alert('비정상적인 접근입니다. 다시 시도해 주세요.');
-        else if (getStatus == '500') alert('서버 내부 오류입니다. 다시 시도해 주세요.');
+
+            var variable = window.consultingResult = await inputCustData(formData),
+                getStatus = variable.status;
+
+            if (getStatus == '200') {
+                window.dataLayer.push({
+                    event: "DB_input"
+                });
+                alert('상담신청이 완료되었습니다.');
+            } else if (getStatus == '403') alert('비정상적인 접근입니다. 다시 시도해 주세요.');
+            else if (getStatus == '500') alert('서버 내부 오류입니다. 다시 시도해 주세요.');
+        })
     }
 
     // Get the modal
