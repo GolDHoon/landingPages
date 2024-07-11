337d45cac36a26b383731ac1cdd6f060fde65f1f
손철훈 <sch9208@gmail.com>
2024-07-11 11:59:09 +0900

    케이파이넌스 수정

--- k-finance.co.kr/js/common.js
+++ k-finance.co.kr/js/common.js
@@ -125,13 +125,13 @@ document.addEventListener("DOMContentLoaded", function() {
         var variable = window.consultingResult = await window.inputCustData(formData),
             getStatus = variable.status;
 
-        if (getStatus === '200') {
+        if (getStatus === 200) {
             window.dataLayer.push({
                 event: "DB_input"
             });
             alert('상담신청이 완료되었습니다.');
-        } else if (getStatus === '403') alert('비정상적인 접근입니다. 다시 시도해 주세요.');
-        else if (getStatus === '500') alert('서버 내부 오류입니다. 다시 시도해 주세요.');
+        } else if (getStatus === 403) alert('비정상적인 접근입니다. 다시 시도해 주세요.');
+        else if (getStatus === 500) alert('서버 내부 오류입니다. 다시 시도해 주세요.');
     }
 
     // Get the modal
