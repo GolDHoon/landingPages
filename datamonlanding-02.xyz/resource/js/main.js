fd9282a9b8ea179c761bcc270f4c9a23509a96fd
손철훈 <sch9208@gmail.com>
2024-08-14 19:02:00 +0900

    블루밍 임시

---
+++ datamonlanding-02.xyz/resource/js/main.js
@@ -0,0 +1,15 @@
+
+
+
+window.addEventListener("load", function() {
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+});
+
+window.addEventListener("resize", function () {
+})
+
+window.addEventListener("scroll", function () {
+})
