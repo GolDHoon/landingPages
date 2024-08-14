578374402b23767683a464b9688c1328550c2080
손철훈 <sch9208@gmail.com>
2024-08-14 19:17:25 +0900

    블루밍 임시

--- datamonlanding-02.xyz/resource/js/sub.js
+++ datamonlanding-02.xyz/resource/js/sub.js
@@ -116,6 +116,7 @@ function imgReSetting() {
     let topper_img_src = topper_img.src.substring(0, topper_img.src.indexOf("/resource/img/") + "/resource/img/".length);
     let date_count_img = document.querySelector(".date_count_img");
     let date_count_img_src = date_count_img.src.substring(0, date_count_img.src.indexOf("/resource/img/") + "/resource/img/".length);
+    alert(body.offsetWidth)
     if(body.offsetWidth > 767){
         switch (date.getDay()){
             case 0:
