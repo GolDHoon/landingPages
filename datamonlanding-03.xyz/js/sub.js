06f89d9dd45cf03a5a29defd6522ccde23982abb
손철훈 <sch9208@gmail.com>
2024-08-26 15:41:39 +0900

    테스트

---
+++ datamonlanding-03.xyz/js/sub.js
@@ -0,0 +1,15 @@
+// 인풋폼 이동 함수
+function toInputForm (event) {
+    document.querySelector('.section-04').scrollIntoView({
+        behavior: 'smooth' // 스크롤을 부드럽게 이동하게 설정
+    });
+}
+
+// 전화번호 발리데이션
+function phoneValidation(event){
+    if(event.target.value.length > 7){
+        event.target.value = previousValue;
+    }else {
+        previousValue = event.target.value;
+    }
+}
