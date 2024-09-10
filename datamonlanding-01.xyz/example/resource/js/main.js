53ac915bcbe9c66b51669e19bd72dc007bb9e4e2
손철훈 <sch9208@gmail.com>
2024-09-10 17:00:46 +0900

    참조페이지 수정

--- datamonlanding-01.xyz/example/resource/js/main.js
+++ datamonlanding-01.xyz/example/resource/js/main.js
@@ -1,16 +1,4 @@
-function navOnclick(event){
-    event.preventDefault();
-    if(event.target.closest('ul').getAttribute('data-nav-active-type') === 'active-class'){
-        document.querySelectorAll(".nav-link").forEach(node => {
-            node.classList.remove("active");
-        })
-        event.target.classList.add("active");
-    }
 
-    if(event.target.closest('ul').getAttribute('data-nav_click_action') === 'scroll'){
-        document.querySelector("#" + event.target.closest('li').getAttribute('data-nav-point')).scrollIntoView({ behavior: 'smooth' });
-    }
-}
 
 document.addEventListener("DOMContentLoaded", () => {
 
