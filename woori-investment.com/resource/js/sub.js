9d93993a7a4ff7ee399ca18c57de381039fddefd
손철훈 <sch9208@gmail.com>
2024-09-12 12:40:19 +0900

    우리투자 퍼블리싱 수정

--- woori-investment.com/resource/js/sub.js
+++ woori-investment.com/resource/js/sub.js
@@ -28,3 +28,29 @@ document.getElementById('agree_modal_on').addEventListener('click', function() {
     var myModal = new bootstrap.Modal(document.getElementById('policyModal'));
     myModal.show();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rightNavFixed = document.getElementById('right_nav_fixed');
+    const targetElement = document.getElementById('con06_wrapper');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0 // Adjust as needed for when you want to trigger the effect
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                rightNavFixed.classList.remove('fade-in');
+                rightNavFixed.classList.add('fade-out');
+            } else {
+                rightNavFixed.classList.remove('fade-out');
+                rightNavFixed.classList.add('fade-in');
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    observer.observe(targetElement);
+});
