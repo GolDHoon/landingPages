018f77cfb20926ac66fd2b5a3589c7656d92d0fa
ireh1214 <pir@driven.co.kr>
2024-09-23 13:58:56 +0900

    테스트 코드 리셋(2)

--- datamonlanding-02.xyz/resource/js/main.js
+++ datamonlanding-02.xyz/resource/js/main.js
@@ -23,9 +23,9 @@ async function submit(formData) {
     const getStatus = variable.status;
 
     if (getStatus === 200) {
-        window.dataLayer.push({
-            event: "DB_input"
-        });
+        // window.dataLayer.push({
+        //     event: "DB_input"
+        // });
         alert('상담신청이 완료되었습니다.');
     } else if (getStatus === 403) {
         alert('비정상적인 접근입니다. 다시 시도해 주세요.');
