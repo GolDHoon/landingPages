61d4f9ae8c9084cb021b7b70528be1a24cc6afb0
sonyeonkyung <thsdusrud00@gmail.com>
2024-09-23 13:35:44 +0900

    테스트 코드 리셋

---
+++ datamonlanding-02.xyz/resource/js/main.js
@@ -0,0 +1,39 @@
+// 전역 변수 설정
+window.input_data = [];
+window.input_allow = false;
+window.input_disable_message = "";
+
+// 함수 정의
+function closeExampleDescirption() {
+    document.querySelectorAll(".example_description").forEach(node => {
+        node.style.display = "none";
+    });
+}
+
+function dbInput() {
+    if (window.input_allow) {
+        submit(window.input_data).then(r => {});
+    } else {
+        alert(window.input_disable_message);
+    }
+}
+
+async function submit(formData) {
+    const variable = window.consultingResult = await window.inputCustData(formData);
+    const getStatus = variable.status;
+
+    if (getStatus === 200) {
+        window.dataLayer.push({
+            event: "DB_input"
+        });
+        alert('상담신청이 완료되었습니다.');
+    } else if (getStatus === 403) {
+        alert('비정상적인 접근입니다. 다시 시도해 주세요.');
+    } else if (getStatus === 500) {
+        alert('서버 내부 오류입니다. 다시 시도해 주세요.');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // DOMContentLoaded 이벤트 핸들러
+});
---
+++ datamonlanding-02.xyz/resource/js/sub.js
@@ -0,0 +1,150 @@
+// main.js에서 설정한 전역 변수 사용
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+function goInputForm() {
+    document.querySelector("#footer").scrollIntoView({ behavior: 'smooth' });
+}
+
+// main.js의 dbInput 함수를 사용할 수 있음
+function onCLickDBInput() {
+    const agree = document.querySelector("#agree");
+    const name = document.querySelector("#name");
+    const phone2 = document.querySelector("#phone2");
+    const phone3 = document.querySelector("#phone3");
+
+    if (!!window.blockedKeyword) {
+        const blockK = window.blockedKeyword.data;
+        let isBreak = false;
+
+        blockK.forEach(function (word) {
+            if (name.value.includes(word)) {
+                isBreak = true;
+                window.input_disable_message = '차단된 키워드가 사용되었습니다. 다시 입력해 주세요. (' + name.value + ')';
+                name.focus();
+            }
+        });
+
+        if (isBreak === true) {
+            return validationBreakerResult(true);
+        }
+    }
+
+    if (name.value === '' || name.value === 'undefined') {
+        window.input_disable_message = '이름을 입력해주세요.';
+        name.focus();
+        return validationBreakerResult(true);
+    } else {
+        const cho_sung_result = /^[ㄱ-ㅎ]/.test(name.value);
+        if (!cho_sung_result) {
+            if (!/^[가-힣]{2,7}$/.test(name.value)) {
+                window.input_disable_message = '초성을 제외한 한글 2~7글자를 입력해주세요.';
+                name.focus();
+                return validationBreakerResult(true);
+            }
+        } else {
+            window.input_disable_message = '초성을 제거해주세요.';
+            name.focus();
+            return validationBreakerResult(true);
+        }
+    }
+
+    if (2000 > Number(phone2.value) || phone2.value.length !== 4) {
+        window.input_disable_message = '010을 제외한 휴대폰 번호를 정확히 입력해주세요.';
+        phone2.focus();
+        return validationBreakerResult(true);
+    }
+
+    if (phone3.value.length !== 4) {
+        window.input_disable_message = '010을 제외한 휴대폰 번호를 정확히 입력해주세요.';
+        phone3.focus();
+        return validationBreakerResult(true);
+    }
+
+    if (!agree.checked) {
+        window.input_disable_message = '개인정보처리방침에 동의해주세요.';
+        agree.focus();
+        return validationBreakerResult(true);
+    }
+
+    validationBreakerResult(false);
+}
+
+function validationBreakerResult(bool) {
+    if (bool) {
+        window.input_allow = false;
+        dbInput();
+        return false;
+    } else {
+        const name = document.querySelector("#name");
+        const phone2 = document.querySelector("#phone2");
+        const phone3 = document.querySelector("#phone3");
+
+        window.input_data = [
+            { "key": "성함", "value": name.value },
+            { "key": "번호", "value": "010" + phone2.value + phone3.value }
+        ];
+        window.input_allow = true;
+        dbInput();
+        return true;
+    }
+}
+
+function inputValidation(event) {
+    if (
+        event.target.value.length >= 4 &&
+        event.key !== 'Backspace' && event.key !== 'Delete' &&
+        event.key !== 'ArrowUp' && event.key !== 'ArrowDown' &&
+        event.key !== 'ArrowLeft' && event.key !== 'ArrowRight' &&
+        event.key !== 'Tab'
+    ) {
+        event.preventDefault();
+    }
+}
+
+document.getElementById('agree_modal_on').addEventListener('click', function () {
+    const myModal = new bootstrap.Modal(document.getElementById('policyModal'));
+    myModal.show();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rightNavFixed = document.getElementById('right_nav_fixed');
+    const targetElement = document.getElementById('con06_wrapper');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                rightNavFixed.classList.remove('fade-in');
+                rightNavFixed.classList.add('fade-out');
+            } else {
+                rightNavFixed.classList.remove('fade-out');
+                rightNavFixed.classList.add('fade-in');
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    observer.observe(targetElement);
+});
