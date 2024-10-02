bf9d7f67fdb3caf36d32587915c8ff93c4ba10ce
ireh1214 <pir@driven.co.kr>
2024-10-02 10:22:19 +0900

    스탠탑비뇨기과의원 요청사항 반영

--- mansmedical.com/resource/js/common.js
+++ mansmedical.com/resource/js/common.js
@@ -52,6 +52,12 @@ function consultingButton () {
         return false;
     }
 
+    // if (1020000000 > Number(phone.value) || phone.value.length !== 11) {
+    //     window.input_disable_message = '010을 제외한 휴대폰 번호를 정확히 입력해주세요.';
+    //     phone.focus();
+    //     return validationBreakerResult(true);
+    // }
+
     if(!isAgree){
         alert('개인정보처리방침에 동의해주세요.');
         document.querySelector('.wr_9').focus();
@@ -63,8 +69,7 @@ function consultingButton () {
         {"key": "번호", "value": "010"+phone}
     ];
 
-    submit(formData).then(() => {});
-
+    submit(formData).then(() => {})
 }
 
 async function submit(formData) {
@@ -76,13 +81,13 @@ async function submit(formData) {
             event: "DB_input"
         });
         alert('상담신청이 완료되었습니다.');
+        location.reload(true);
     } else if (getStatus === 403) alert('비정상적인 접근입니다. 다시 시도해 주세요.');
     else if (getStatus === 500) alert('서버 내부 오류입니다. 다시 시도해 주세요.');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    let modal =document.querySelector('#myModal');
-
+    let modal = document.querySelector('#myModal');
     let span = document.querySelector("#modal_view");
 
     span.onclick = function() {
@@ -94,10 +99,25 @@ document.addEventListener("DOMContentLoaded", function() {
             modal.style.display = 'none';
         }
     }
-
     document.querySelector(".close").onclick = function (evnet) {
         modal.style.display = 'none';
     }
 });
+
+// 24.10.2 추가 / 휴대폰번호 글자수 제한
+function inputOnKeyDownValidation(event) {
+    if (
+        event.target.value.length >= 8 &&
+        event.key !== 'Backspace' && event.key !== 'Delete' &&
+        event.key !== 'ArrowLeft' && event.key !== 'ArrowRight' &&
+        event.key !== 'Tab'
+    ) {
+        event.preventDefault();
+    }
+    if(event.key === 'ArrowUp' || event.key === 'ArrowDown'){
+        event.preventDefault();
+    }
+}
+//end
 
 
